f84bc14660b6bb9912ef571c9dad5a6421cd4488
Trock <g_trock@163.com>
2021-09-09 10:06:55 +0800

    fix: test race

--- consul/consul_test.go
+++ consul/consul_test.go
@@ -1,71 +1,81 @@
-package core_sd_test
+package consul_test
 
 import (
 	"context"
 	"fmt"
+	"github.com/ggxxll/core-sd/consul"
+	"github.com/go-kit/kit/endpoint"
 	"io"
 	"os"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/DoNewsCode/core"
+	"github.com/DoNewsCode/core/contract"
 	"github.com/DoNewsCode/core/di"
-	"github.com/DoNewsCode/core/otetcd"
+	"github.com/DoNewsCode/core/srvgrpc"
 	"github.com/DoNewsCode/core/srvhttp"
 
 	core_sd "github.com/ggxxll/core-sd"
-	"github.com/ggxxll/core-sd/etcd"
-
-	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/sd"
-	kitetcd "github.com/go-kit/kit/sd/etcd"
 	"github.com/go-kit/kit/sd/lb"
+	"github.com/hashicorp/consul/api"
 )
 
-func TestEtcdRegistrar(t *testing.T) {
-	if os.Getenv("ETCD_ADDR") == "" {
-		fmt.Println("set ETCD_ADDR to run test")
+func provideConsulClient(conf contract.ConfigAccessor) (*api.Client, error) {
+	return api.NewClient(&api.Config{
+		Address: conf.String("consul.addr"),
+	})
+}
+
+func TestConsulRegistrar(t *testing.T) {
+	if os.Getenv("CONSUL_ADDR") == "" {
+		fmt.Println("set CONSUL_ADDR to run test")
 		return
 	}
+
 	serverIp := os.Getenv("SERVER_IP")
 	if serverIp == "" {
 		serverIp = "127.0.0.1"
 	}
 
 	c := core.Default(
-		core.WithInline("name", "consul_test"),
+		core.WithInline("name", "foo"),
 		core.WithInline("version", "0.0.1"),
 		core.WithInline("http.addr", serverIp+":8888"),
-		core.WithInline("grpc.disable", true),
-		core.WithInline("etcd.default.endpoints", strings.Split(os.Getenv("ETCD_ADDR"), ",")),
+		core.WithInline("grpc.addr", serverIp+":9999"),
+		core.WithInline("consul.addr", os.Getenv("CONSUL_ADDR")),
 	)
 	defer c.Shutdown()
 
-	c.Provide(etcd.Providers())
-	c.Provide(otetcd.Providers())
+	c.Provide(consul.Providers())
 	c.Provide(di.Deps{
-		func() *etcd.RegistrarOptions {
-			return &etcd.RegistrarOptions{Service: kitetcd.Service{
-				Key:   fmt.Sprintf("/services/foosvc/%s:8888", serverIp),
-				Value: fmt.Sprintf("http://%s:8888/live", serverIp),
-			}}
-		},
-		func() *etcd.InstancerOption {
-			return &etcd.InstancerOption{Prefix: "/services/foosvc"}
+		provideConsulClient,
+	})
+	c.Provide(di.Deps{
+		func(appName contract.AppName, conf contract.ConfigAccessor) *consul.InstancerOption {
+			return &consul.InstancerOption{
+				Service: appName.String(),
+				Tags: []string{
+					fmt.Sprintf("version=%s", conf.String("version")),
+				},
+				PassingOnly: false,
+			}
 		},
 	})
 
 	c.AddModuleFunc(core_sd.NewRegistrarModule)
 	c.AddModule(srvhttp.HealthCheckModule{})
+	c.AddModule(srvgrpc.HealthCheckModule{})
 
-	ctx, canel := context.WithCancel(context.Background())
-	defer canel()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	go func() {
 		_ = c.Serve(ctx)
 	}()
-	time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
+
 	c.Invoke(func(in sd.Instancer, logger log.Logger) {
 		endpointer := sd.NewEndpointer(in, barFactory, logger)
 		balancer := lb.NewRoundRobin(endpointer)
---
+++ etcd/etcd_test.go
@@ -0,0 +1,83 @@
+package etcd_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/ggxxll/core-sd/etcd"
+	"io"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/DoNewsCode/core"
+	"github.com/DoNewsCode/core/di"
+	"github.com/DoNewsCode/core/otetcd"
+	"github.com/DoNewsCode/core/srvhttp"
+
+	core_sd "github.com/ggxxll/core-sd"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/sd"
+	kitetcd "github.com/go-kit/kit/sd/etcd"
+	"github.com/go-kit/kit/sd/lb"
+)
+
+func TestEtcdRegistrar(t *testing.T) {
+	if os.Getenv("ETCD_ADDR") == "" {
+		fmt.Println("set ETCD_ADDR to run test")
+		return
+	}
+	serverIp := os.Getenv("SERVER_IP")
+	if serverIp == "" {
+		serverIp = "127.0.0.1"
+	}
+
+	c := core.Default(
+		core.WithInline("name", "consul_test"),
+		core.WithInline("version", "0.0.1"),
+		core.WithInline("http.addr", serverIp+":8888"),
+		core.WithInline("grpc.disable", true),
+		core.WithInline("etcd.default.endpoints", strings.Split(os.Getenv("ETCD_ADDR"), ",")),
+	)
+	defer c.Shutdown()
+
+	c.Provide(etcd.Providers())
+	c.Provide(otetcd.Providers())
+	c.Provide(di.Deps{
+		func() *etcd.RegistrarOptions {
+			return &etcd.RegistrarOptions{Service: kitetcd.Service{
+				Key:   fmt.Sprintf("/services/foosvc/%s:8888", serverIp),
+				Value: fmt.Sprintf("http://%s:8888/live", serverIp),
+			}}
+		},
+		func() *etcd.InstancerOption {
+			return &etcd.InstancerOption{Prefix: "/services/foosvc"}
+		},
+	})
+
+	c.AddModuleFunc(core_sd.NewRegistrarModule)
+	c.AddModule(srvhttp.HealthCheckModule{})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		_ = c.Serve(ctx)
+	}()
+	time.Sleep(1 * time.Second)
+	c.Invoke(func(in sd.Instancer, logger log.Logger) {
+		endpointer := sd.NewEndpointer(in, barFactory, logger)
+		balancer := lb.NewRoundRobin(endpointer)
+		retry := lb.Retry(3, 3*time.Second, balancer)
+
+		// And now retry can be used like any other endpoint.
+		req := struct{}{}
+		if _, err := retry(ctx, req); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func barFactory(string) (endpoint.Endpoint, io.Closer, error) {
+	return endpoint.Nop, nil, nil
+}
